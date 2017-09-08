9d8ebc31a705c50f27ad67b1e2c905f404d19e96
inada daiki <inada.daiki@m.alhinc.jp>
2017-09-08 15:54:12 +0900

    test

---
+++ FristExample.java
@@ -0,0 +1,12 @@
+class FristExample {
+	public static void main(String[] args) {
+		int i;
+		int n;
+		for( i = 1; i <= 9; i++){
+			for( n = 1; n <= 9; n++){
+				System.out.print( i * n + " ");
+			}
+			System.out.println("");
+		}
+	}
+}
